eccd2e4beaa9582a5c85319d046c68f11d01c106
uLemos <fsnvag12@gmail.com>
2025-06-21 10:05:05 -0300

    :memmo: Arquitetura de diretórios criada

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div className='flex min-h-svh flex-col items-center justify-center'>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default App
