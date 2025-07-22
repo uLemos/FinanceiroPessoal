87ad9457ce999c6df5770fb739a8aa8bf56cdc9e
uLemos <fsnvag12@gmail.com>
2025-07-22 13:48:10 -0300

    :sparkles: adicionando o novo padrão de rota nos patterns do CORS

--- backend/src/main/java/com/financeiro/backend/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/com/financeiro/backend/infrastructure/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     http
       .csrf(csrf -> csrf.disable())
       .authorizeHttpRequests( auth -> auth
-        .requestMatchers("/auth/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+        .requestMatchers("/v1/api/auth/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
         .anyRequest().authenticated()
       )
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
